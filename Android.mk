cf13591d2fbd10880369ed48fca815318e68ab3c
darkobas <darkobas@gmail.com>
2015-08-05 16:16:57 +0200

    give opnedelta system uid
    Change-Id: Idbba1e7322ca084d6b2b425cf992f0603e863704

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_PACKAGE_NAME := OpenDelta
 LOCAL_MODULE_TAGS := optional
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_CERTIFICATE := platform
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
